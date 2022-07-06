afa000d8d550789f0d78c781add4994e521783fd
AnderSanchezUPV <ander.sanchez@ehu.eus>
2022-07-06 11:56:52 +0200

    Fix CUDA bug by reducing MiniBatchSize

--- dummy_params.m
+++ dummy_params.m
@@ -1,6 +1,6 @@
 %%          Dummy params
 clear params
 params.convergencia="Concat";
-params.Filter_Size=[5,5];
+params.Filter_Size=[3,3];
 params.LearnRateDropFactor=0.5;
 params.InitialLearnRate=1e-3;
